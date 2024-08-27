e19f20b526b2a165fed4654011ae8072e81cf1e5
marcoweb <marcocarvalho.web@gmail.com>
2024-08-27 18:48:55 +0000

    Token Service

--- app/src/main/java/application/service/TokenService.java
+++ app/src/main/java/application/service/TokenService.java
@@ -1,10 +1,15 @@
 package application.service;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 import org.springframework.stereotype.Service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import application.model.Usuario;
 
@@ -18,9 +23,27 @@ public class TokenService {
             return JWT.create()
                 .withIssuer("API Tarefas")
                 .withSubject(usuario.getNomeDeUsuario())
+                .withExpiresAt(expirationDate())
                 .sign(algorithm);
         } catch (JWTCreationException exception) {
             throw new RuntimeException("Erro ao gerar JWT", exception);
         }
+    }
+
+    public String getSubject(String tokenJWT) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                .withIssuer("API Tarefas")
+                .build()
+                .verify(tokenJWT)
+                .getSubject();
+        } catch (JWTVerificationException exception) {
+            throw new RuntimeException("Token Inválido ou Expirado");
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }
